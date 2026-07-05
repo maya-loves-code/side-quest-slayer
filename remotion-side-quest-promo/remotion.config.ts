21875c287c0e3020c81cbbf8ea0e4bea4b51cd5f
themayabello <themayabello@gmail.com>
2026-07-05 16:15:34 -0700

    Address Remotion promo review comments

--- remotion-side-quest-promo/remotion.config.ts
+++ remotion-side-quest-promo/remotion.config.ts
@@ -1,5 +1,5 @@
 /**
- * Note: When using the Node.JS APIs, the config file
+ * Note: When using the Node.js APIs, the config file
  * doesn't apply. Instead, pass options directly to the APIs.
  *
  * All configuration options: https://remotion.dev/docs/config
